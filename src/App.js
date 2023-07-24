c66c1f786fd98dce2e8033510294c67c7f041a59
DanielDeda <daniel.costa.deda.oliveira@gmail.com>
2023-07-24 19:01:39 -0300

    created new function 'createCard'

--- src/App.js
+++ src/App.js
@@ -2,12 +2,21 @@ import Avatar from "./components/avatar";
 import Card from "./components/card";
 import contacts from "./components/contacts";
 
+function createCard(contact) {
+  return <Card 
+  name= {contact.name}
+  imgURL = {contact.imgURL}
+  phone = {contact.phone}
+  email = {contact.email} />
+}
+
 function App() {
   return (
     <div>
       <h1 className="heading">My Contacts</h1>
       <Avatar imgURL = 'https://cdn.iset.io/assets/55268/produtos/966/adesivo-para-geladeira-homer-simpson-com-cerveja-3.jpg'/>
-      <Card 
+      {contacts.map(createCard)}
+      {/* <Card 
         name={contacts[0].name}
         imgURL={contacts[0].imgURL}
         phone={contacts[0].phone}
@@ -24,7 +33,7 @@ function App() {
         imgURL={contacts[2].imgURL}
         phone={contacts[2].phone}
         email={contacts[2].email}
-      />
+      /> */}
     </div>
   );
 }
