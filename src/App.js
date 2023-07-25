4e3a71d06f95e354148541b9fba11f0935dfa3b6
DanielDeda <daniel.costa.deda.oliveira@gmail.com>
2023-07-25 16:17:49 -0300

    refactored

--- src/App.js
+++ src/App.js
@@ -17,24 +17,6 @@ function App() {
       <h1 className="heading">My Contacts</h1>
       <Avatar imgURL = 'https://cdn.iset.io/assets/55268/produtos/966/adesivo-para-geladeira-homer-simpson-com-cerveja-3.jpg'/>
       {contacts.map(createCard)}
-      {/* <Card 
-        name={contacts[0].name}
-        imgURL={contacts[0].imgURL}
-        phone={contacts[0].phone}
-        email={contacts[0].email}
-      />
-      <Card 
-        name={contacts[1].name}
-        imgURL={contacts[1].imgURL}
-        phone={contacts[1].phone}
-        email={contacts[1].email}
-      />
-      <Card 
-        name={contacts[2].name}
-        imgURL={contacts[2].imgURL}
-        phone={contacts[2].phone}
-        email={contacts[2].email}
-      /> */}
     </div>
   );
 }
